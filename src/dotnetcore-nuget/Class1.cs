c4f3fff821052aa212fd22b87564179ba7ccba74
IlyaFinkelshteyn <ilyaf@appveyor.com>
2016-11-14 20:49:44 -0800

    Initial commit

---
+++ src/dotnetcore-nuget/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnetcore_nuget
+{
+    public class Class1
+    {
+        public Class1()
+        {
+        }
+    }
+}
---
+++ src/dotnetcore-nuget/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("dotnetcore_nuget")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9e3cfbd5-a82c-4cb3-8d73-c2ca6b1a122d")]
